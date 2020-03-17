aeba24880cb85e7be7bdb6068a17db1d6c823301
kevin-it237 <ngaleuabel@gmail.com>
2020-03-17 09:57:15 +0100

    Change url

--- config/rootUrl.js
+++ config/rootUrl.js
@@ -1,6 +1,6 @@
 module.exports = {
-    rootUrl: 'http://localhost:3000',
-    //rootUrl: 'https://entrecops.co',
-    apiUrl: 'http://localhost:5000',
-    //apiUrl: 'https://entrecops.co'
+    //rootUrl: 'http://localhost:3000',
+    rootUrl: 'https://survey-cmr.cf',
+    //apiUrl: 'http://localhost:5000',
+    apiUrl: 'https://survey-cmr.herokuapp.com'
 }
