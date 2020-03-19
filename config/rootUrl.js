30731a6314cdec43f39e3ce37f9a9ea0017ac828
kevin-it237 <ngaleuabel@gmail.com>
2020-03-19 12:30:16 +0100

    upload images on firebase storage finished: and change url

--- config/rootUrl.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    //rootUrl: 'http://localhost:3000',
-    rootUrl: 'https://survey-cmr.cf',
-    //apiUrl: 'http://localhost:5000',
-    apiUrl: 'https://survey-cmr.herokuapp.com'
-}
