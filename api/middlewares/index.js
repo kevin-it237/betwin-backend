502de4cf499d5a49d0bc5e3cae37b857cfd6c2c9
kevin-it237 <ngaleuabel@gmail.com>
2022-10-27 23:23:52 +0200

    reset git index

--- api/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-const authJwt = require("./checkAuth");
-
-module.exports = {
-    authJwt,
-};
--- config/database.js
+++
@@ -1,2 +0,0 @@
-const localDB = "mongodb://127.0.0.1/betwin";
-const liveDB = "mongodb+srv://betwin-admin:betwin@123@cluster0.tvrqc.mongodb.net/betwin?retryWrites=true&w=majority"
