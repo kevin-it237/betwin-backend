358a34bc4bf1d8e234a48c1970f4ddc32eda19d7
kevin-it237 <ngaleuabel@gmail.com>
2022-10-28 00:48:03 +0200

    Update database url

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const localDB = "mongodb://localhost/betwin";
-const liveDB = "mongodb://admin:admin5@ds329668.mlab.com:29668/betwin"
+const liveDB = "mongodb+srv://betwin:yBOOzFXLBP39B0ni@cluster0.tvrqc.mongodb.net/?retryWrites=true&w=majority"
 
 module.exports = {
     database: process.env.NODE_ENV !== 'production' ? localDB : liveDB
