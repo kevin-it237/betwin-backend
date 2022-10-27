d4821fd98f76749f9df79554c7a986705ac0e79c
kevin-it237 <ngaleuabel@gmail.com>
2022-10-28 00:16:44 +0200

    test

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose.Promise = global.Promise
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function() {
-    console.log('Connected to mongodb');
+    console.log('Connected to mongodb', config.database);
 })
 
 // App initialization
